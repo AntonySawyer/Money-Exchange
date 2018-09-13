428d071f0cacb11f38b84b57a54d2c36d5b9fb4c
Antony <A.Krutenok@gmail.com>
2018-09-13 16:03:19 +0300

    add feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    var coins = ['H', 'Q', 'D', 'N', 'P'];
+    var denomination = [50, 25, 10, 5, 1];
+    var count = new Object();
+
+    if (currency > 10000) {
+    	count.error = "You are rich, my friend! We don't have so much coins for exchange";
+    } else {
+  		for (var i = 0; i < coins.length; i++) {
+    		counter(coins[i], denomination[i]);
+  		}    	
+    }
+
+	function counter(coin, denomination){
+		if (currency != 0 && currency >= denomination) {		
+			count[coin] = parseInt(currency / denomination);
+			currency = currency % denomination;
+		}
+	}
+
+	return count;
 }
