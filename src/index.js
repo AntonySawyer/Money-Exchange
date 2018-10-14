10ae569bb0b4bf830f4d927c4f7ede9ba0b3b956
Antony <A.Krutenok@gmail.com>
2018-10-14 18:13:05 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
+  const coins = ['H', 'Q', 'D', 'N', 'P'];
+  const denomination = [50, 25, 10, 5, 1];
+  const count = {};
 
-    let coins = ['H', 'Q', 'D', 'N', 'P'];
-    let denomination = [50, 25, 10, 5, 1];
-    let count = new Object();
+  if (currency > 10000) {
+    count.error = "You are rich, my friend! We don't have so much coins for exchange";
+  } else {
+    coins.forEach((item, index) => counter(item, denomination[index]));
+  }
 
-    if (currency > 10000) {
-    	count.error = "You are rich, my friend! We don't have so much coins for exchange";
-    } else {
-  		for (let i = 0; i < coins.length; i++) {
-    		counter(coins[i], denomination[i]);
-  		}
+  function counter(coin, denomination){
+    if (currency >= denomination) {		
+      count[coin] = parseInt(currency / denomination);
+      currency = currency % denomination;
     }
+  }
 
-	function counter(coin, denomination){
-		if (currency > 0 && currency >= denomination) {		
-			count[coin] = parseInt(currency / denomination);
-			currency = currency % denomination;
-		}
-	}
-
-	return count;
+  return count;
 }
